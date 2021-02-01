f1ab2789907180dc8b93a2b4145bf72bd2f297bb
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-02 00:41:20 +0600

    Null Vs Undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,31 @@
+//===Undefined==//
+let ages;
+console.log(ages);
+
+//another
+function add(num1, num2){
+    console.log(num2);  
+}
+const result = add(13);
+console.log(result);  // If we don't pass the parameters, it's shows undefined.
+
+//another
+function sum(num1, num2){
+    console.log(num1, num2);
+}
+const result2 = add(13);
+console.log(result); // If we don't pass the parameters, it's shows undefined.
+
+//another
+const items = {names:"rahim", phone: "01782323"};
+console.log(items.gf);
+
+//another
+let fun = undefined;
+console.log(fun);
+
+//===End Undefined==//
+
+//===Null==//
+
+//null means nothing. It is supposed to be something that doesn't exist.
