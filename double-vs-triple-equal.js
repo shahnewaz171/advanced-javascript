d52374286cf26b0ac3b333426a6902738007765b
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-02 00:56:56 +0600

    Null Vs Undefined

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,56 @@
+//===Double Equal===//   //It's check only value
+const first = 2;
+const second = 2;
+if(first == second){
+    console.log("Condition is true");
+}
+else{
+    console.log('Condition is false');
+}
+
+//another      //It's check only value
+const first2 = 2;
+const second2 = "2";
+if(first2 == second2){
+    console.log("Condition is true");
+}
+else{
+    console.log('Condition is false');
+}
+
+
+//another      //It's check only value
+const third = 1;
+const fourth = true;
+if(third == fourth){
+    console.log("Condition is true");
+}
+else{
+    console.log('Condition is false');
+}
+
+//===Double Equal===//
+
+
+
+//===Triple Equal===//           //Its check both value and types
+const first3 = 2;
+const second3 = "2";
+if(first3 === second3){
+    console.log("Condition is true");
+}
+else{
+    console.log('Condition is false');
+}
+
+//another          //Its check both value and types
+const third2 = 1;
+const fourth2 = true;
+if(third === fourth){
+    console.log("Condition is true");
+}
+else{
+    console.log('Condition is false');
+}
+
+//===End Triple Equal===//    
