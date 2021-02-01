4fb6a64fd80d493e7c1964223f94df6f243b1421
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-02 00:11:40 +0600

    Truthy and Falsy values

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//Falsy:
+//false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+//Truthy:
+//"0", " ", [], {}, 'false'
+
+let name = 'false';
+if(name){
+    console.log('Condition is true');
+}
+else{
+    console.log('Condition is false');
+}
