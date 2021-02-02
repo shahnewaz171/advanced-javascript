a183640f809cdd2b1068cfb27ba0189faffa978b
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-02 16:50:30 +0600

    Array slice, splice, array join elements

---
+++ array-split.js
@@ -0,0 +1,24 @@
+//===slice method===//
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+const part = nums.slice(2, 5);
+console.log(part);
+//===End slice method===//
+
+
+
+//===splice method===//
+const nums2 = [1, 2, 3, 4, 5, 6, 7, 8];
+const removed = nums2.splice(2,5);
+console.log(removed);
+console.log(nums2);
+//===End splice method===//
+
+
+
+//===Array Join elements===//
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const join = numbers.join('newaz');
+console.log(join);
+//===End Array Join elements===//
+
+
